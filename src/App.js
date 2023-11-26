c85241592b675e2a4118f53d7fe004c629d1171c
alessandrodiaz <alessdg02@gmail.com>
2023-11-26 11:18:45 -0500

    Actualización de los estilos a Twind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,35 +6,36 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "./connections/apollo";
-import "tailwindcss/tailwind.css";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <ToastContainer
-          position="bottom-right"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-          bodyClassName={"toastBody"}
-        />
+	return (
+		<BrowserRouter>
+			<ApolloProvider client={client}>
+				<ToastContainer
+					position="bottom-right"
+					autoClose={2000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="colored"
+					bodyClassName={"toastBody"}
+				/>
 
-        {/* <GoogleOAuthProvider clientId="176512858558-8jjm4bclhonv3vbi2dlg22djslfm7iti.apps.googleusercontent.com"> */}
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-        {/* </GoogleOAuthProvider> */}
-      </ApolloProvider>
-    </BrowserRouter>
-  );
+				{/* <GoogleOAuthProvider clientId="176512858558-8jjm4bclhonv3vbi2dlg22djslfm7iti.apps.googleusercontent.com"> */}
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="" element={<Login />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+				{/* </GoogleOAuthProvider> */}
+			</ApolloProvider>
+		</BrowserRouter>
+	);
 };
 
 export default App;
--- src/authentication-app-authentication-project.js
+++ src/authentication-app-authentication-project.js
@@ -2,20 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import App from "./App";
-// import Root from "./root.component2"
+import { setup } from "twind";
 
-// const Root = () => {
-// 	return <App />;
-// };
+setup({
+	theme: {
+		fontFamily: {
+			text: ["Gabarito", "sans-serif"],
+			serif: ["Times", "serif"],
+		},
+		extend: {
+			colors: {
+				"custom-orange": "#fb923c",
+			},
+		},
+	},
+
+	preflight: {
+		"@import": `url('https://fonts.googleapis.com/css2?family=Gabarito&display=swap')`,
+	},
+});
 
 const lifecycles = singleSpaReact({
-  React,
-  ReactDOM,
-  rootComponent: App,
-  errorBoundary(err, info, props) {
-    // Customize the root error boundary for your microfrontend here.
-    return null;
-  },
+	React,
+	ReactDOM,
+	rootComponent: App,
+	errorBoundary(err, info, props) {
+		// Customize the root error boundary for your microfrontend here.
+		return null;
+	},
 });
 
 export const { bootstrap, mount, unmount } = lifecycles;
